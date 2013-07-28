911f210e9d558bd254ff9012f1093fc79858c628
Ahmed Yiwere <myyiwere@gmail.com>
2013-07-28 13:50:17 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = app=express();
 var fs = require('fs');
 buf =new Buffer(256);
-buf = fs.readFileSync(index.html')
+buf = fs.readFileSync('index.html')
 app.use(express.logger());
 
 app.get('/', function(request, response) {
