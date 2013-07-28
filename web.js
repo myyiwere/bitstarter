1c6fcc4dfe9b517e7c38abb9d13a39fece226047
Ahmed Yiwere <myyiwere@gmail.com>
2013-07-28 13:29:46 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var app = express();
-fs = require('fs');
+var app = app=express();
+var fs = require('fs');
 buf =new Buffer(256);
 buf = fs.readFileSync('/home/ahmed/bitstarter/index.html')
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.end(buf.toString());
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
