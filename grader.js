f9e5bd9bb0d075306eb7ccfa57cd2a9df731c98c
Ahmed Yiwere <myyiwere@gmail.com>
2013-08-06 22:45:33 +0000

    Edited Prod Desc

--- grader.js
+++ grader.js
@@ -26,7 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var rest = require('restler')
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,10 +65,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <webpage_url>', 'Path to webpage')
         .parse(process.argv);
+    if(program.url){
+                    rest.get(program.url).on('complete', function(result){
+                    if (result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+                this.retry(5000);} // try again after 5 sec
+     else {
+                fs.writeFileSync('outfile.html', result)
+    var checkJson = checkHtmlFile('outfile.html', program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);}
+     });
+}
+
+     else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+    console.log(outJson);}
+}
+ else {
     exports.checkHtmlFile = checkHtmlFile;
 }
